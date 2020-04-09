7b47fb188d72692faae2d35888586b0d10d7f5d4
Cristiano Clemente <32751490+cpcmc@users.noreply.github.com>
2020-04-09 18:01:36 +0100

    beautify

--- background.js
+++ background.js
@@ -3,45 +3,60 @@ var run_clicked = false;
 var save_clicked = false;
 var cancel_clicked = false;
 var bar_active = false;
+var to_be_deleted = [];
 
 chrome.downloads.onCreated.addListener(function(downloadItem) {
-    if(!downloadItem.finalUrl.endsWith(".exe")) {
-      chrome.downloads.setShelfEnabled(true);
-      return;
-    }
+  console.log(downloadItem);
+  if (!downloadItem.finalUrl.endsWith(".exe")) {
+    chrome.downloads.setShelfEnabled(true);
+    return;
+  }
 
-    if(!bar_active) {
-      chrome.downloads.setShelfEnabled(false);
-      chrome.downloads.cancel(downloadItem.id);
-      current_url = downloadItem.url;
-      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {"name": "create-div"});
+  if (!bar_active) {
+    chrome.downloads.setShelfEnabled(false);
+    chrome.downloads.cancel(downloadItem.id);
+    current_url = downloadItem.url;
+    chrome.tabs.query({
+      active: true,
+      currentWindow: true
+    }, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {
+        "name": "create-div"
       });
-      bar_active = true;
-      return;
-    }
+    });
+    bar_active = true;
+    return;
+  }
 
-    if(!run_clicked && !save_clicked) {
-      chrome.downloads.setShelfEnabled(false);
-      chrome.downloads.cancel(downloadItem.id);
-      return;
-    }
+  if (!run_clicked && !save_clicked) {
+    chrome.downloads.setShelfEnabled(false);
+    chrome.downloads.cancel(downloadItem.id);
+    return;
+  }
 
-    //normal download
-    run_clicked = false;
-    save_clicked = false;
+  //normal download
+  run_clicked = false;
+  save_clicked = false;
 });
 
 chrome.runtime.onMessage.addListener(function(message) {
   if (message.name == "run-clicked") {
     run_clicked = true;
-    chrome.downloads.download({"url": current_url, "saveAs": false});
+    chrome.downloads.download({
+      "url": current_url
+    });
     return;
   }
 
   if (message.name == "save-clicked") {
     save_clicked = true;
-    chrome.downloads.download({"url": current_url});
+    chrome.downloads.download({
+      "url": current_url
+    }, function(downloadId) {
+      if (downloadId != undefined)
+        current_url = "";
+      bar_active = false;
+    });
     return;
   }
 
--- content.js
+++ content.js
@@ -1,46 +1,52 @@
 chrome.runtime.onMessage.addListener(function(message) {
-        if (message.name == "create-div") {
-            var div = document.createElement("div");
-            div.id = "div-fixed"
-            div.style.position = "fixed";
-            div.style.left = "0";
-            div.style.bottom = "0";
-            div.style.width = "100%";
-            div.style.backgroundColor = "black";
-            div.style.color = "white";
-            div.style.textAlign = "center";
-            div.style.width = "100%";
-            div.style.height = "50px";
+  if (message.name == "create-div") {
+    var div = document.createElement("div");
+    div.id = "div-fixed"
+    div.style.position = "fixed";
+    div.style.left = "0";
+    div.style.bottom = "0";
+    div.style.width = "100%";
+    div.style.backgroundColor = "black";
+    div.style.color = "white";
+    div.style.textAlign = "center";
+    div.style.width = "100%";
+    div.style.height = "50px";
 
-            var run = document.createElement("button");
-            run.id = "button-run";
-            run.onclick = function() {
-              console.log("run!");
-              chrome.runtime.sendMessage({"name": "run-clicked"});
-            }
-            run.innerHTML = "Run";
-            div.appendChild(run);
+    var run = document.createElement("button");
+    run.id = "button-run";
+    run.onclick = function() {
+      console.log("run!");
+      chrome.runtime.sendMessage({
+        "name": "run-clicked"
+      });
+    }
+    run.innerHTML = "Run";
+    div.appendChild(run);
 
-            var save = document.createElement("button");
-            save.id = "button-save";
-            save.onclick = function() {
-              console.log("save!");
-              chrome.runtime.sendMessage({"name": "save-clicked"});
-            }
-            save.innerHTML = "Save";
-            div.appendChild(save);
+    var save = document.createElement("button");
+    save.id = "button-save";
+    save.onclick = function() {
+      console.log("save!");
+      chrome.runtime.sendMessage({
+        "name": "save-clicked"
+      });
+    }
+    save.innerHTML = "Save";
+    div.appendChild(save);
 
-            var cancel = document.createElement("button");
-            cancel.id = "button-cancel";
-            cancel.onclick = function() {
-              console.log("cancel!");
-              var element = document.getElementById("div-fixed");
-              element.parentNode.removeChild(element);
-              chrome.runtime.sendMessage({"name": "cancel-clicked"});
-            }
-            cancel.innerHTML = "X";
-            div.appendChild(cancel);
+    var cancel = document.createElement("button");
+    cancel.id = "button-cancel";
+    cancel.onclick = function() {
+      console.log("cancel!");
+      var element = document.getElementById("div-fixed");
+      element.parentNode.removeChild(element);
+      chrome.runtime.sendMessage({
+        "name": "cancel-clicked"
+      });
+    }
+    cancel.innerHTML = "X";
+    div.appendChild(cancel);
 
-            document.body.appendChild(div);
-        }
+    document.body.appendChild(div);
+  }
 });
